11a41597b15418fed06a8805cd8af45b874994ea
karthik <pkarthikbt@gmail.com>
2023-01-24 17:47:01 +1100

    frontend.sh

---
+++ frontend.sh
@@ -0,0 +1,10 @@
+yum install nginx -y
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/niginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/static/*
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+systemctl enable nginx
+systemctl start nginx
